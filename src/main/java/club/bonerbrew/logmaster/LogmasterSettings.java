63a2c6e4663945a12827c839c2210565a6f20003
chrishansen69 <cadrekucra@gmx.com>
2017-03-24 20:57:21 -0300

    add docs for LogmasterSettings
    (cherry picked from commit a558d0dfb089d5c4d039bf60bcc1c4006340af42)

--- src/main/java/club/bonerbrew/logmaster/LogmasterSettings.java
+++ src/main/java/club/bonerbrew/logmaster/LogmasterSettings.java
@@ -14,32 +14,70 @@ public class LogmasterSettings {
         this.outputToFile = HLogger.outputToFile;
         this.outputFine = HLogger.outputFine;
     }
+    /**
+     * Create a new Logmaster builder
+     * @return the LogmasterSettings instance
+     */
     public static LogmasterSettings c() {
+        return new LogmasterSettings();
+    }
+    /**
+     * Create a new Logmaster builder
+     * @return the LogmasterSettings instance
+     */
+    public static LogmasterSettings setup() {
         return new LogmasterSettings();
     }
     public void apply() {
         HLogger.applySettings(this);
     }
+    /**
+     * Set output to stdout
+     * @param b true to output to stdout, false to suppress
+     * @return this
+     */
     public LogmasterSettings stdOut(boolean b) {
         this.stdOut=b;
         return this;
     }
+    /**
+     * Enable output to stdout
+     * @return this
+     */
     public LogmasterSettings stdOut() {
         this.stdOut=true;
         return this;
     }
+    /**
+     * Set output to stderr
+     * @param b true to output to stderr, false to suppress
+     * @return this
+     */
     public LogmasterSettings stdErr(boolean b) {
         this.stdErr=b;
         return this;
     }
+    /**
+     * Enable output to stderr
+     * @return this
+     */
     public LogmasterSettings stdErr() {
         this.stdErr=true;
         return this;
     }
+    /**
+     * Set output to file
+     * @param b true to output to file, false to suppress
+     * @return this
+     */
     public LogmasterSettings outputToFile(boolean b) {
         this.outputToFile=b;
         return this;
     }
+    /**
+     * Enable output to file
+     * @return this
+     */
     public LogmasterSettings outputToFile() {
         this.outputToFile=true;
         return this;
